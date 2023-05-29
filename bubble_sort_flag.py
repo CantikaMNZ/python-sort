376bcbc28bdb1dfbe6189a2dd4126017e8e74b3e
CantikaMNZ <133628457+CantikaMNZ@users.noreply.github.com>
2023-05-29 11:21:30 +0700

    Update bubble_sort_flag.py

--- bubble_sort_flag.py
+++ bubble_sort_flag.py
@@ -6,7 +6,9 @@ def bubble_sort(arr):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
                 is_sorted = False
-
+        if is_sorted:
+            break
+        
 arr = [64, 34, 25, 12, 22, 11, 90]
 bubble_sort(arr)
 print("Hasil pengurutan:", arr)
